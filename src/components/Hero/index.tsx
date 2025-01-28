ac98e497e4966d92c50f0dccb54101a345631108
AlbertJohnson994 <112587686+AlbertJohnson994@users.noreply.github.com>
2025-01-28 11:38:59 -0300

    changing css to styled component

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,17 +1,3 @@
-// import styles from './Hero.module.css'
-
-// const Hero = () => (
-//   <form className={styles.form}>
-//     <div className="container">
-//       <h2 className={styles.heroTitle}>
-//         As melhores vagas para tecnologia, design e artes visuais.
-//       </h2>
-//     </div>
-//   </form>
-// )
-
-// export default Hero
-
 import React from 'react'
 import { FormStyled, ContainerStyled, HeroTitleStyled } from './styles'
 
